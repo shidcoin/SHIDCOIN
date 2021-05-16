022e75e2937ed2a36d62d6f7765ef1b5c27177b7
SHID <84255581+shidcoin@users.noreply.github.com>
2021-05-15 22:14:33 -0700

    Update SHID-20.sol

--- SHID-20.sol
+++ SHID-20.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract SHIDDED is ERC20 {
-    constructor () public ERC20 ("SHIDDED", "SHID"){
+    constructor () ERC20 ("SHIDDED", "SHID"){
         _mint(msg.sender, 1000000000000000000000000000);
     }
 }
